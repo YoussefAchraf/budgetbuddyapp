26b72e4cdc9c9ce6af8cbcbf32f82793f2918365
YoussefAchraf <135220512+YoussefAchraf@users.noreply.github.com>
2024-04-08 21:23:12 +0200

    new commit

--- client/src/pages/login/LogIn.js
+++ client/src/pages/login/LogIn.js
@@ -1,28 +1,41 @@
-// LogIn.js
 import React, { useState } from 'react';
-import { useLoginMutation } from '../../api/UsersApi';
+import axios from 'axios';
 import UsrInpt from "../../components/usrinpt/UsrInpt";
 import Styles from "./LogIn.module.css";
 
 const LogIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [login, { isLoading, isError }] = useLoginMutation();
+  const [UsrEm, setEmail] = useState('');
+  const [UsrPwd, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
   
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await login({ email, password }).unwrap();
-      console.log('Login successful:', data);
+      const response = await axios.post('https://budgetbuddyapp.onrender.com/login', { UsrEm, UsrPwd });
+      console.log('Login successful:', response.data);
+      
+      // Store JWT token in local storage
+      localStorage.setItem('token', response.data.token);
+      
       // Handle successful login
     } catch (error) {
-      console.error('Login error:', error);
-      // Handle login error
+      console.error('Login error:', error.response);
+      if (error.response && error.response.data) {
+        // Map error messages to corresponding input fields
+        const newErrors = {};
+        if (error.response.data === 'Email and password are required') {
+          newErrors.UsrEm = 'Email is required';
+          newErrors.UsrPwd = 'Password is required';
+        } else if (error.response.data === 'Incorrect password') {
+          newErrors.UsrPwd = 'Incorrect password';
+        }
+        setErrors(newErrors);
+      } else {
+        // Handle other errors
+        setErrors({ general: 'An error occurred. Please try again later.' });
+      }
     }
   };
-
-  // Check if the app is running as a PWA
-  const isPWA = window.matchMedia('(display-mode: standalone)').matches;
 
   return (
     <section className={Styles.LogIn}>
@@ -32,15 +45,17 @@ const LogIn = () => {
           InptId="UsrEm"
           InptLb="Email"
           InptTp="text"
-          value={email}
+          value={UsrEm}
           onChange={(e) => setEmail(e.target.value)} // Update email state
+          error={errors && errors.UsrEm}
         />
         <UsrInpt 
           InptId="UsrPwd"
           InptLb="Password"
           InptTp="password"
-          value={password}
+          value={UsrPwd}
           onChange={(e) => setPassword(e.target.value)} // Update password state
+          error={errors && errors.UsrPwd}
         />
         <button type="submit">Log in</button>
       </form>
